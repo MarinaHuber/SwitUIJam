beb2db9e439df2875deeef2ffd7c12f423705905
Marina Huber <huber.marina@gmail..com>
2021-11-05 20:26:54 +0100

    project setup

---
+++ SwiftUIJam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIJam
+//
+//  Created by Marina Huber on 05.11.2021..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+                .previewDevice("iPhone 13")
+        }
+    }
+}
---
+++ SwiftUIJam/SwiftUIJamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIJamApp.swift
+//  SwiftUIJam
+//
+//  Created by Marina Huber on 05.11.2021..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIJamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
