4d3569a9047d8abd5a61e80f46531915beba2f04
Marina Huber <huber.marina@gmail.com>
2021-11-05 20:50:22 +0100

    testing command line

--- SwiftUIJam/ContentView.swift
+++ SwiftUIJam/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             ContentView()
-                .previewDevice("iPhone 13")
+                .previewDevice("iPhone 12")
         }
     }
 }
